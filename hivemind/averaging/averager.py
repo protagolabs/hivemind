675a29c80e5ac5aba118e01917722b87561558e3
yuanjie.mao <yuanjie.mao@protagonist-ai.com>
2023-02-20 15:26:18 +0800

    revert old code

--- hivemind/averaging/averager.py
+++ hivemind/averaging/averager.py
@@ -722,7 +722,56 @@ class DecentralizedAverager(mp.Process, ServicerBase):
         return future.result(timeout=timeout) if wait else future
 
     async def _load_state_from_peers(self, future: MPFuture, timeout: Optional[float] = None):
-        await _load_state_from_peers(self, future, timeout)
+        if timeout is not None:
+            timeout = self.next_chunk_timeout if self.next_chunk_timeout is not None else self.request_timeout
+        try:
+            key_manager = self._matchmaking.group_key_manager
+            peer_priority, _ = self.dht.get(f"{key_manager.prefix}.all_averagers", latest=True) or ({}, None)
+            peer_priority = {
+                PeerID(peer_id): (float(info.value), random.random())  # using randomness as a tie breaker
+                for peer_id, info in peer_priority.items()
+                if isinstance(info, ValueWithExpiration) and isinstance(info.value, (float, int))
+            }
+
+            if not isinstance(peer_priority, dict) or len(peer_priority) == 0:
+                logger.info(f"Averager could not load state from peers: peer dict empty or corrupted {peer_priority}")
+                future.set_result(None)
+                return
+
+            metadata = None
+            for peer in sorted(peer_priority.keys(), key=peer_priority.get, reverse=True):
+                if peer != self.peer_id:
+                    logger.info(f"Downloading parameters from peer {peer}")
+                    try:
+                        stub = self.get_stub(self._p2p, peer, namespace=self.prefix)
+                        stream = await stub.rpc_download_state(averaging_pb2.DownloadRequest())
+                        current_tensor_parts, tensors = [], []
+
+                        # TODO merge this with hivemind.compression.deserialize_tensor_stream
+                        async for message in aiter_with_timeout(stream, timeout=timeout):
+                            if message.metadata:
+                                metadata = self.serializer.loads(message.metadata)
+                            if message.tensor_part.dtype and current_tensor_parts:
+                                # tensor_part.dtype indicates the start of the new tensor, so we should wrap up this one
+                                tensors.append(deserialize_torch_tensor(combine_from_streaming(current_tensor_parts)))
+                                current_tensor_parts = []
+                            current_tensor_parts.append(message.tensor_part)
+                        if current_tensor_parts:
+                            tensors.append(deserialize_torch_tensor(combine_from_streaming(current_tensor_parts)))
+
+                        if not metadata:
+                            logger.debug(f"Peer {peer} did not send its state")
+                            continue
+
+                        logger.info(f"Finished downloading state from {peer}")
+                        future.set_result((metadata, tensors))
+                        return
+                    except Exception as e:
+                        logger.exception(f"Failed to download state from {peer} - {repr(e)}")
+
+        finally:
+            if not future.done():
+                future.set_result(None)
 
     def get_group_bits(self, wait: bool = True):
         """
